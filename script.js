3d668e1f1df8112623de74b1a839cb95b330af25
Matt <matthewjpetroff@gmail.com>
2020-08-29 20:41:41 -0400

    Modify JS

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let canvas;
 let canvasContext;
-let direction = null;
-let snake = [randomSpot()];
+let direction;
+let snake;
 let applePosition;
-let score = 0;
+let score;
+let highScore = 0;
+let playing = true;
 
 function randomSpot() {
 	const x = Math.floor(Math.random() * Math.floor(40)) * 20;
@@ -34,7 +36,7 @@ function updateSnake() {
 	}
 }
 
-function updateApplePosition() {
+function setApplePosition() {
 	while (true) {
 		applePosition = randomSpot();
 		if (appleNotOnSnake()) {
@@ -90,55 +92,88 @@ function drawSnake() {
 
 function incrementScore() {
 	score++;
-	console.log(score);
+	document.getElementById('scoreboard').textContent = `Score: ${score} High Score: ${highScore}`;
 }
 
-function gameOver() {
-	if (snake[0][0] > 780 || snake[0][0] < 0 || snake[0][1] > 580 || snake[0][1] < 0) {
-		console.log('Out of bounds');
-	}
+function updateHighScore() {
+	highScore = score;
+	document.getElementById('scoreboard').textContent = `Score: ${score} High Score: ${highScore}`;
+}
 
-	for (let i = 1; i < snake.length; i++) {
+function snakeHitsWall() {
+	if (snake[0][0] > 780 || snake[0][0] < 0 || snake[0][1] > 580 || snake[0][1] < 0) {
+		return true;
+	}
+	return false;
+}	
+	
+function snakeHitsSelf() {
+	for (let i = 3; i < snake.length; i++) {
 		if (snake[0][0] === snake[i][0] && snake[0][1] === snake[i][1]) {
-			console.log('ran into itself');
+			return true;
 		}
 	}
-}
+	return false;
+}	
 
 function addToSnake() {
 	snake.splice(0, 0, applePosition);	
 }
-
-function updateScreen() {
+function drawBoard() {
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-	updateSnake();
-	drawSnake();
-	gameOver();
-	if (applePosition[0] === snake[0][0] && applePosition[1] === snake[0][1]) {
-		addToSnake();
-		incrementScore();
-		updateApplePosition();
-	} 
-	drawApple();	
-	
 }
 
-document.addEventListener("keydown", function(event) {
+function initializeGame() {
+	direction = null;
+	score = 0;
+	snake = [randomSpot()];
+	drawBoard();
+	drawSnake();
+	setApplePosition();
+	drawApple();
+}
+
+function gameplay() {
+	const snakeEatsApple = (applePosition[0] === snake[0][0] && applePosition[1] === snake[0][1]);
+	drawBoard();
+	updateSnake();
+	drawSnake();
+	if (snakeEatsApple) {
+		addToSnake();
+		incrementScore();
+		setApplePosition();
+	} 
+	drawApple();
+}
+
+document.addEventListener('keydown', function(event) {
 	updateDirection();
 })
+
+document.getElementById('new-game').addEventListener('click', function(e) {
+	if (!displayNum.includes('.')) {
+		updateDisplayNum('.');
+	}
+});
 
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
-	drawSnake();
-	updateApplePosition();
-	drawApple();
-	setInterval(updateScreen, 75);
+	initializeGame();
+
+	let game = setInterval(() => {
+		if (snakeHitsWall() || snakeHitsSelf()) {
+			if (score > highScore) {
+				updateHighScore();
+			}
+			clearInterval(game);
+		}
+		gameplay();
+	}, 100);
 }
 
+	
+
 //To-do
-//1. End game on wall run in
-//2. Update score
-//3. Save high score
-//4. Refactor
+//1. Give player option to play again
